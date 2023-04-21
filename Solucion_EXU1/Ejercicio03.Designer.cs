fce8f7846112f7612b809466277bbe7851de7215
Elvis Mamani Valdivia <elvismv.2016@gmail.com>
2023-04-21 11:30:31 -0500

    Solucion Examen

--- Solucion_EXU1/Ejercicio03.Designer.cs
+++ Solucion_EXU1/Ejercicio03.Designer.cs
@@ -28,12 +28,75 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblMensaje = new System.Windows.Forms.Label();
+            this.pgbRecorrido = new System.Windows.Forms.TextBox();
+            this.btnIniciar = new System.Windows.Forms.Button();
+            this.lblMensa = new System.Windows.Forms.Label();
+            this.bwProgreso = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // lblMensaje
+            // 
+            this.lblMensaje.AutoSize = true;
+            this.lblMensaje.Location = new System.Drawing.Point(278, 162);
+            this.lblMensaje.Name = "lblMensaje";
+            this.lblMensaje.Size = new System.Drawing.Size(0, 13);
+            this.lblMensaje.TabIndex = 1;
+            // 
+            // pgbRecorrido
+            // 
+            this.pgbRecorrido.Location = new System.Drawing.Point(152, 203);
+            this.pgbRecorrido.Name = "pgbRecorrido";
+            this.pgbRecorrido.Size = new System.Drawing.Size(269, 20);
+            this.pgbRecorrido.TabIndex = 2;
+            // 
+            // btnIniciar
+            // 
+            this.btnIniciar.Location = new System.Drawing.Point(268, 86);
+            this.btnIniciar.Name = "btnIniciar";
+            this.btnIniciar.Size = new System.Drawing.Size(75, 23);
+            this.btnIniciar.TabIndex = 3;
+            this.btnIniciar.Text = "INICIAR";
+            this.btnIniciar.UseVisualStyleBackColor = true;
+            this.btnIniciar.Click += new System.EventHandler(this.btnIniciar_Click);
+            // 
+            // lblMensa
+            // 
+            this.lblMensa.AutoSize = true;
+            this.lblMensa.Location = new System.Drawing.Point(285, 161);
+            this.lblMensa.Name = "lblMensa";
+            this.lblMensa.Size = new System.Drawing.Size(35, 13);
+            this.lblMensa.TabIndex = 4;
+            this.lblMensa.Text = "label1";
+            // 
+            // bwProgreso
+            // 
+            this.bwProgreso.WorkerReportsProgress = true;
+            this.bwProgreso.DoWork += new System.ComponentModel.DoWorkEventHandler(this.bwProgreso_DoWork);
+            this.bwProgreso.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.bwProgreso_ProgressChanged);
+            // 
+            // Ejercicio03
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblMensa);
+            this.Controls.Add(this.btnIniciar);
+            this.Controls.Add(this.pgbRecorrido);
+            this.Controls.Add(this.lblMensaje);
+            this.Name = "Ejercicio03";
             this.Text = "Ejercicio03";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblMensaje;
+        private System.Windows.Forms.TextBox pgbRecorrido;
+        private System.Windows.Forms.Button btnIniciar;
+        private System.Windows.Forms.Label lblMensa;
+        private System.ComponentModel.BackgroundWorker bwProgreso;
     }
 }
--- Solucion_EXU1/Ejercicio03.cs
+++ Solucion_EXU1/Ejercicio03.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
+
 
 namespace Solucion_EXU1
 {
@@ -15,6 +17,44 @@ namespace Solucion_EXU1
         public Ejercicio03()
         {
             InitializeComponent();
+        }
+
+        private void bwProgreso_DoWork(object sender, DoWorkEventArgs e)
+        {
+            for (int i = 1; i <=100, i++)
+            {
+                bwProgreso.ReportProgress(i);
+                Thread.Sleep(100);
+
+            }
+        }
+
+        private void bwProgreso_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            if (e.ProgressPercentage >= 0 && e.ProgressPercentage <= 20)
+            {
+                lblMensa.ForeColor = Color.Black;
+                lblMensa.Text = string.Format("progreso ...{0} %", e.ProgressPercentage);
+            }
+            else if (e.ProgressPercentage >= 0 && e.ProgressPercentage <= 50)
+            {
+                lblMensa.ForeColor = Color.Yellow;
+                lblMensa.Text = string.Format("progreso ...{0} %", e.ProgressPercentage);
+            }
+            else if (e.ProgressPercentage >= 0 && e.ProgressPercentage >= 70)
+            {
+                lblMensa.ForeColor = Color.Green;
+                lblMensa.Text = string.Format("progreso ...{0} %", e.ProgressPercentage);
+            }
+            else
+            {
+                lblMensa.ForeColor = Color.Blue;
+                lblMensa.Text = string.Format("progreso ...{0} %", e.ProgressPercentage);
+            }
+
+        private void btnIniciar_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
